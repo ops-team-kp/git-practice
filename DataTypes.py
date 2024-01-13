ba2734502281c87b47700fa76e8d60c9000afafb
KrushnaPimpale <kpimple656@gmail.com>
2024-01-13 20:59:02 +0530

    Revert "First commit"
    This reverts commit 26c8096ab3ec766eb0e36e490287f801cb8c94da.
    
    getting all back

--- DataTypes.py
+++
@@ -1,7 +0,0 @@
-# 1. Import the requests library
-import requests
-URL = "https://transcripts.gotomeeting.com/#/s/db3e20674772c4558a107266e242e37c85a9d77897396a28ed11394663b60c97"
-# 2. download the data behind the URL
-response = requests.get(URL)
-# 3. Open the response into a new file called instagram.ico
-open("AWS", "wb").write(response.content)
--- ec2.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_instance" "my_ec2" {
-    ami = ""
-    instance_type = ""
-
-}
